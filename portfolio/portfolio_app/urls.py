2395c8aee1c61d15967291ffea4f4d669de0590e
dev-tr26 <trangadiarudra26@gmail.com>
2025-07-09 21:45:39 +0530

    updated blogs

--- portfolio/portfolio_app/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path("", views.home_page, name='home'),
-    path("blog/", views.blog_list, name="blog_list"),
-    path("blog/<slug:slug>/", views.blog_detail, name='blog_detail'),
-]
